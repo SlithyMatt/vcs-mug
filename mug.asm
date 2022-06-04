5dfe7821b6b0419b5a2d86628ff41dfcc38b2285
Matt Heffernan <slithymatt@hotmail.com>
2022-06-03 23:15:56 -0400

    complete intro

--- mug.asm
+++ mug.asm
@@ -18,6 +18,9 @@ Clear:
    inx
    bne Clear
 
+   ; Initialize
+   stx FRAME_COUNT
+
    ; Initialize graphics
    lda #0
    sta COLUBK
@@ -25,7 +28,137 @@ Clear:
    lda #60
    sta LINE_NUM
 
-StartOfFrame:
+; Intro Screen
+Intro:
+; Start of vertical blank processing
+   lda #0
+   sta VBLANK
+
+   lda #2
+   sta VSYNC
+
+; 3 scanlines of VSYNCH signal...
+   sta WSYNC
+
+   ldx #$0E
+   stx COLUBK  ; background = white
+   lda #0
+   sta CTRLPF  ; repeat playfield
+
+   ldx #5      ; players = double size
+   stx NUSIZ0
+   stx NUSIZ1
+
+   sta WSYNC
+   sta WSYNC
+
+   sta VSYNC
+
+; 37 scanlines of vertical blank...
+
+   ldx #36
+@vblank_loop:
+   sta WSYNC
+   dex
+   bne @vblank_loop
+
+   sta WSYNC
+   ldx #2
+@p0_hpos_loop:
+   nop
+   dex
+   bne @p0_hpos_loop
+   sta RESP0 ; set player 0 horizontal position (left side of slithy tove)
+   nop
+   nop
+   nop
+   sta RESP1 ; set player 1 horizontal position (right side of slithy tove)
+
+
+; 192 scanlines of picture...
+   ldx #51
+@top_margin:
+   sta WSYNC
+   dex
+   bne @top_margin
+
+@print_intro:
+   ldx #0
+   ldy #0
+@intro_loop:
+   sta WSYNC
+   sty PF0
+   lda intro_pf_color,x
+   sta COLUPF
+   lda intro_pf1_left,x
+   sta PF1
+   lda intro_pf2_left,x
+   sta PF2
+   lda p0_tove_color,x
+   sta COLUP0
+   lda p1_tove_color,x
+   sta COLUP1
+   lda intro_pf0_right,x
+   sta PF0
+   lda intro_pf1_right,x
+   sta PF1
+   lda intro_pf2_right,x
+   sta PF2
+   sta WSYNC
+   sty PF0
+   lda intro_pf_color,x
+   sta COLUPF
+   lda intro_pf1_left,x
+   sta PF1
+   lda intro_pf2_left,x
+   sta PF2
+   lda p0_tove,x
+   sta GRP0
+   lda p1_tove,x
+   sta GRP1
+   lda intro_pf0_right,x
+   sta PF0
+   lda intro_pf1_right,x
+   sta PF1
+   lda intro_pf2_right,x
+   sta PF2
+   inx
+   cpx #47
+   beq @end_pf
+   jmp @intro_loop
+@end_pf:
+   sty PF0
+   sty PF1
+   sty PF2
+   sty GRP0
+   sty GRP1
+
+   ldx #51
+@bottom_margin:
+   sta WSYNC
+   dex
+   bne @bottom_margin
+
+   lda #%01000010
+   sta VBLANK                     ; end of screen - enter blanking
+
+
+   ; 30 scanlines of overscan...
+Intro_Overscan:
+
+   sta WSYNC
+   inc FRAME_COUNT
+   ldx #29
+@oscan_loop:
+   sta WSYNC
+   dex
+   bne @oscan_loop
+   lda FRAME_COUNT
+   cmp #0
+   beq Title
+   jmp Intro
+
+Title:
 
 ; Start of vertical blank processing
    lda #0
@@ -46,9 +179,6 @@ StartOfFrame:
 
    inx
    stx CTRLPF  ; mirror playfield
-
-
-
    sta WSYNC
 
    lda #$9E    ; players = light blue
@@ -88,7 +218,7 @@ StartOfFrame:
 
 
 ; 192 scanlines of picture...
-Picture:
+Title_Picture:
    ldx #25
 @top_margin:
    sta WSYNC
@@ -219,7 +349,7 @@ Picture:
 
 
    ; 30 scanlines of overscan...
-Overscan:
+Title_Overscan:
 
    sta WSYNC
    inc FRAME_COUNT
@@ -239,7 +369,7 @@ Overscan:
    sta WSYNC
    dex
    bne @oscan_loop
-   jmp StartOfFrame
+   jmp Title
 
 ; Pattern Data
 fill_colors:
@@ -271,10 +401,16 @@ p0_tove:
 .byte %00000000
 .byte %00000000
 .byte %00000001
+.repeat 22
+.byte 0
+.endrepeat
 
 p0_tove_color:
 .byte $F6,$F6,$F6,$F6,$F6,$F6,$F6,$F6,$F4,$F4,$F0,$F6,$F6
 .byte $26,$F0,$F0,$F0,$F6,$F8,$FC,$E4,$F4,$F4,$F4,$F4
+.repeat 22
+.byte $0E
+.endrepeat
 
 p1_tove:
 .byte %00000001
@@ -302,89 +438,250 @@ p1_tove:
 .byte %01100000
 .byte %01100000
 .byte %11000000
+.repeat 22
+.byte 0
+.endrepeat
+
 
 p1_tove_color:
 .byte $0A,$0A,$0A,$F8,$F8,$F8,$F8,$F6,$F6,$F6,$0A,$0A,$0A
 .byte $0A,$04,$04,$04,$F0,$F2,$E4,$E4,$F0,$F0,$E4,$E4
-
-intro_pf0_left:
-.repeat 36
-.byte %00000000
+.repeat 22
+.byte $0E
 .endrepeat
-.byte %11000000
-.byte %01000000
-.byte %01000000
-.byte %11000000
-.byte %01000000
-.byte %01000000
-.byte %01000000
 
 intro_pf1_left:
 .byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %00000001
+.byte %00000011
+.byte %00000010
+.byte %00000010
+.byte %00000011
+.byte %00000001
+.byte %00000000
+.byte %00000010
+.byte %00000011
 .byte %00000001
 .byte %00000000
 .byte %00000000
 .byte %00000000
-.byte %00000100
-.byte %00001110
-.byte %00001010
-.byte %00001000
+.byte %00000000
+.byte %00000000
 .byte %00001100
-.byte %00000110
-.byte %00000010
-.byte %00001010
-.byte %00001110
-.byte %00000100
-.byte %00000000
-.byte %00000000
-.byte %00000000
-.byte %00000000
-.byte %00000000
-.byte %00110000
-.byte %01111001
-.byte %11001001
-.byte %10000001
-.byte %10000011
-.byte %10111010
-.byte %10111010
-.byte %10001011
-.byte %10001011
-.byte %11001010
-.byte %01111010
+.byte %00011110
 .byte %00110010
+.byte %00100000
+.byte %00100000
+.byte %00101110
+.byte %00101110
+.byte %00100010
+.byte %00100010
+.byte %00110010
+.byte %00011110
+.byte %00001100
 .byte %00000000
 .byte %00000000
 .byte %00000000
 .byte %00000000
-.byte %00110011
+.byte %11001100
 .byte %10101010
 .byte %10101010
-.byte %00110011
-.byte %00101010
-.byte %00101010
-.byte %00101011
+.byte %11001100
+.byte %10001010
+.byte %10001010
+.byte %10001010
 
 intro_pf2_left:
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %01000100
+.byte %01000110
+.byte %01010100
+.byte %01010100
+.byte %01000100
+.byte %11000100
+.byte %11010101
+.byte %01010101
+.byte %01010100
+.byte %01010100
+.byte %01010101
+.byte %01010101
+.byte %01010101
+.byte %11010101
+.byte %10010100
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %01000100
+.byte %01001110
+.byte %11001110
+.byte %11001010
+.byte %11011011
+.byte %01010001
+.byte %01010001
+.byte %01011111
+.byte %01011111
+.byte %01010001
+.byte %01010001
+.byte %01010001
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %00100111
+.byte %01010001
 .byte %00010001
-.byte %10010001
-.byte %10010101
-.byte %10010101
-.byte %10110001
-.byte %10110101
-.byte %10010101
-.byte %10010101
-.byte %10010101
-.byte %10010101
-.byte %10010101
-.byte %10010101
-.byte %10110101
-.byte %10100101
-.byte %00000000
-.byte %00000000
-.byte %00000000
-.byte %00000000
-.byte %00000000
+.byte %00100011
+.byte %01000001
+.byte %01010001
+.byte %00100111
 
+intro_pf0_right:
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %11000000
+.byte %11100000
+.byte %00100000
+.byte %00100000
+.byte %00100000
+.byte %01100000
+.byte %11100000
+.byte %10100000
+.byte %10100000
+.byte %10100000
+.byte %10100000
+.byte %10100000
+.byte %10100000
+.byte %10100000
+.byte %10100000
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %01000000
+.byte %01000000
+.byte %01100000
+.byte %01100000
+.byte %01110000
+.byte %01010000
+.byte %01010000
+.byte %01010000
+.byte %01000000
+.byte %01000000
+.byte %01000000
+.byte %01000000
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %01110000
+.byte %00010000
+.byte %00010000
+.byte %00110000
+.byte %00010000
+.byte %00010000
+.byte %01110000
+
+intro_pf1_right:
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %01010000
+.byte %01010000
+.byte %01010000
+.byte %01010000
+.byte %01010000
+.byte %01010000
+.byte %01010000
+.byte %00110000
+.byte %00010000
+.byte %00010000
+.byte %01110000
+.byte %01100000
+.byte %00000000
+.byte %11110011
+.byte %11110111
+.byte %10000100
+.byte %10000100
+.byte %10000100
+.byte %11100111
+.byte %11100011
+.byte %10000000
+.byte %10000000
+.byte %10000100
+.byte %11110111
+.byte %11110011
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %10010111
+.byte %11010010
+.byte %11010010
+.byte %10110010
+.byte %10110010
+.byte %10010010
+.byte %10010010
+
+intro_pf2_right:
+.repeat 25
+.byte %00000000
+.endrepeat
+.byte %00000001
+.byte %00000001
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %00000001
+.byte %00000001
+.byte %00000001
+.byte %00000001
+.byte %00000001
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %00000100
+.byte %00001010
+.byte %00000010
+.byte %00000100
+.byte %00001000
+.byte %00001010
+.byte %00000100
+
+intro_pf_color:
+.repeat 23
+.byte $C8
+.endrepeat
+.repeat 13
+.byte $00
+.endrepeat
+.repeat 11
+.byte $42
+.endrepeat
 
 p0_mug_shape:
 .byte %00001000
